1ddb8a36742eb2c3b859bd8fdae605be9ea34fcf
Hable Khafi <134847663+HableKhafi@users.noreply.github.com>
2025-12-01 17:06:37 +0800

    .

--- Pertmuan3/Modul11/app/views/templates/footer.php
+++ Pertmuan3/Modul11/app/views/templates/footer.php
@@ -1,5 +1,5 @@
 </div> <footer style="text-align: center; margin-top: 30px; padding: 15px; background-color: #e9ecef; border-top: 1px solid #dee2e6;">
-    <p>© 2024 Framework MVC Sederhana | Modul 9 View</p>
+    <p>© 2025 CRUD Mahasiswa | Modul 11</p>
 </footer>
 
 <script>
